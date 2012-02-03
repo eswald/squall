dfd86c61c2c060d3dbf35554404079260430bc19
Eric Wald <eswald@gmail.com>
2012-02-02 17:03:07 -0700

    Launching what could be a database query system.

---
+++ README.txt
@@ -0,0 +1,24 @@
+Squall version 0.1a: Visual database query interface
+
+Related to MySQL Administrator, SQLyog, and Navicat,
+but tailored to my personal whims.
+
+Official source at https://github.com/eswald/squall
+Development roadmap at https://trello.com/board/squall/4f287f8289c9bdd9360f04e3
+
+Copyright 2012 Eric Wald <eswald@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this work except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+I will entertain feature requests at USD$500 per hour, bugfix requests at
+USD$250 per hour.
---
+++ setup.py
@@ -0,0 +1,65 @@
+r'''Squall installation script
+    Usage: python setup.py install
+    
+    This script relies on setuptools and/or distribute.
+'''#"""#'''
+
+# Copyright 2012 Eric Wald
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup
+
+try:
+    readme = open('README.txt').read()
+except:
+    # Todo: Attempt to find the README file.
+    readme = None
+
+setup(
+    # Provided items
+    name = "Squall",
+    version = "0.1a",
+    packages = ["squall"],
+    entry_points = {
+        "console_scripts": [
+        ],
+        "gui_scripts": [
+        ],
+    },
+    
+    # Project metadata
+    author = "Eric Wald",
+    author_email = "eswald@gmail.com",
+    description = "Visual database query interface",
+    long_description = readme,
+    license = "Apache License, Version 2.0",
+    keywords = "database sql mysql gui",
+    url = "https://github.com/eswald/squall",
+    platforms = "Any",
+    classifiers = [
+        "Development Status :: 1 - Planning",
+        "Environment :: X11 Applications",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Topic :: Database :: Front-Ends",
+    ],
+    
+    # Installation options
+    zip_safe = True,
+    install_requires = ["distribute"],
+)
