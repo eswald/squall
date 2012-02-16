e6a42f986d194d88c09e89e58096155d68856ffe
Eric Wald <eswald@gmail.com>
2012-02-16 09:29:22 -0700

    Cleaning up the tree interface somewhat, and preparing the model to look more like the database structure.

--- squall/treepane.py
+++ squall/treepane.py
@@ -17,30 +17,18 @@ r'''Squall schema tree implementation.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from PyQt4.QtCore import QAbstractItemModel, QModelIndex, QString, QVariant, Qt
+from PyQt4.QtCore import QAbstractItemModel, QModelIndex, Qt
 
 class TreeItem(object):
-    def __init__(self, data, parent = 0):
+    def __init__(self, data, parent = None):
         self.parentItem = parent
         self.itemData = data
-        self.childItems = []
-    
-    def appendChild(self, child):
-        self.childItems.append(child)
-    
-    def child(self, row):
-        return self.childItems[row]
-    
-    def childCount(self):
-        return len(self.childItems)
+        self.children = list(self.collect())
     
     def row(self):
         if self.parentItem:
-            return self.parentItem.childItems.index(self)
+            return self.parentItem.children.index(self)
         return 0
-    
-    def columnCount(self):
-        return len(self.itemData)
     
     def data(self, column):
         try:
@@ -50,19 +38,44 @@ class TreeItem(object):
     
     def parent(self):
         return self.parentItem
+    
+    def collect(self):
+        r'''Find any children of this node.
+            May involve database queries.
+        '''#"""#'''
+        for n in range(1,3):
+            yield Server(["Server "+str(n)], self)
+
+class Server(TreeItem):
+    def collect(self):
+        for n in range(1,3):
+            yield Schema(["Schema "+str(n)], self)
+
+class Schema(TreeItem):
+    def collect(self):
+        for n in range(1,5):
+            yield Table(["Table "+str(n)], self)
+
+class Table(TreeItem):
+    def collect(self):
+        for n in range(1,8):
+            yield Column(["Column "+str(n), "int(10) unsigned not null default 1"], self)
+
+class Column(TreeItem):
+    def collect(self):
+        return []
 
 class TreeModel(QAbstractItemModel):
     def __init__(self, data, parent = None):
         QAbstractItemModel.__init__(self, parent)
-        self.rootItem = TreeItem(["Title", "Summary"])
-        self.setupData(data.split("\n"))
+        self.rootItem = TreeItem(["Object", "Notes"])
     
     def index(self, row, column, parent):
         if not self.hasIndex(row, column, parent):
             return QModelIndex()
         
         parentItem = parent.internalPointer() if parent.isValid() else self.rootItem
-        childItem = parentItem.child(row)
+        childItem = parentItem.children[row]
         return self.createIndex(row, column, childItem) if childItem else QModelIndex()
     
     def parent(self, index):
@@ -79,21 +92,14 @@ class TreeModel(QAbstractItemModel):
         if parent.column() > 0:
             return 0
         parentItem = parent.internalPointer() if parent.isValid() else self.rootItem
-        return parentItem.childCount()
+        return len(parentItem.children)
     
     def columnCount(self, parent):
-        parentItem = parent.internalPointer() if parent.isValid() else self.rootItem
-        return parentItem.columnCount()
+        return 2
     
     def data(self, index, role):
-        if not index.isValid():
-            return QVariant()
-        
-        if role != Qt.DisplayRole:
-            return QVariant()
-        
-        item = index.internalPointer()
-        return item.data(index.column())
+        if index.isValid() and role == Qt.DisplayRole:
+            return index.internalPointer().data(index.column())
     
     def flags(self, index):
         if not index.isValid():
@@ -104,101 +110,8 @@ class TreeModel(QAbstractItemModel):
     def headerData(self, section, orientation, role):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             return self.rootItem.data(section)
-        
-        return QVariant()
-    
-    def setupData(self, lines):
-        parents = [self.rootItem]
-        indentations = [0]
-        
-        for line in lines:
-            data = line.strip()
-            if data:
-                position = line.find(data[0])
-                columns = [QString(s.strip()) for s in data.split("  ") if s.strip()]
-                if position > indentations[-1]:
-                    if parents[-1].childCount() > 0:
-                        parents.append(parents[-1].child(parents[-1].childCount() - 1))
-                        indentations.append(position)
-                else:
-                    while position < indentations[-1] and parents:
-                        parents.pop()
-                        indentations.pop()
-                
-                parents[-1].appendChild(TreeItem(columns, parents[-1]))
-
-sample_data = r'''
-Server 1
-    Schema 1
-        Table 1
-            Column 1    int(10) default null
-            Column 2    varchar(80)
-            Column 3    blob
-            Column 4    tinytext not null
-        Table 2
-            Column 1
-            Column 2
-            Column 3
-            Column 4
-    Schema 2
-        Table 1
-            Column 1    int(10) default null
-            Column 2    varchar(80)
-            Column 3    blob
-            Column 4    tinytext not null
-        Table 2
-            Column 1
-            Column 2
-            Column 3
-            Column 4
-    Schema 3
-        Table 1
-            Column 1    int(10) default null
-            Column 2    varchar(80)
-            Column 3    blob
-            Column 4    tinytext not null
-        Table 2
-            Column 1
-            Column 2
-            Column 3
-            Column 4
-Server 2
-    Schema 1            More power to you
-        Table 1         InnoDB
-            Column 1    int(10) default null
-            Column 2    varchar(80)
-            Column 3    blob
-            Column 4    tinytext not null
-        Table 2         45 rows
-            Column 1
-            Column 2
-            Column 3
-            Column 4
-    Schema 2
-        Table 1
-            Column 1    int(10) default null
-            Column 2    varchar(80)
-            Column 3    blob
-            Column 4    tinytext not null
-        Table 2
-            Column 1
-            Column 2
-            Column 3
-            Column 4
-    Schema 3
-        Table 1
-            Column 1    int(10) default null
-            Column 2    varchar(80)
-            Column 3    blob
-            Column 4    tinytext not null
-        Table 2
-            Column 1
-            Column 2
-            Column 3
-            Column 4
-'''#"""#'''
 
 def populate(view):
-    model = TreeModel(sample_data)
+    model = TreeModel(None)
     view.setModel(model)
     return model
